0c7bdd30f05b7f8ff504d69ffe64422920895295
SDuboc <60201084+SDuboc@users.noreply.github.com>
2020-10-03 14:32:27 -0300

    TDD - Corrige implementações

--- test/processador/ProcessadorTest.java
+++ test/processador/ProcessadorTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.*;
 import pagamento.Pagamento;
 import pagamento.TipoPagamentoEnum.TipoPagamento;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
@@ -15,7 +16,8 @@ import static org.junit.Assert.*;
 
 public class ProcessadorTest {
 
-    List<Boleto> listBoletos;
+    private List<Boleto> listBoletos;
+    private static final Date DATE = new Date(System.currentTimeMillis());
 
     @BeforeAll
     public static void printInicioTest() {
@@ -30,28 +32,35 @@ public class ProcessadorTest {
 
     @BeforeEach
     public void inicializa() {
-        inicializaBoletos();
+        List<Boleto> listBoletos = new ArrayList<>();
+        Date date = new Date(System.currentTimeMillis());
+        Boleto boleto = new Boleto("bol123", date, 300);
+        Boleto boleto2 = new Boleto("eto456", date, 100);
+        listBoletos.addAll(Arrays.asList(boleto, boleto2));
     }
 
-    @DisplayName("Testa a verificação correta de uma Fatura paga")
+    @DisplayName("Testa a verificaï¿½ï¿½o correta de uma Fatura paga")
     @Test
     public void testVerificaFaturaPaga() {
         Fatura fatura = inicializaFaturaDeAmbientePago();
         Processador processador = inicializaProcessador(listBoletos, fatura);
         assertTrue(processador.verificaFaturaPaga());
+        System.out.println(LocalDate.now());
     }
 
-    @DisplayName("Testa a verificação correta de uma Fatura não paga")
+    @DisplayName("Testa a verificaï¿½ï¿½o correta de uma Fatura nï¿½o paga")
     @Test
     public void testVerificaFaturaEmAberto() {
-        Fatura fatura = inicializaFaturaDeAmbientePago();
+        Fatura fatura = inicializaFaturaDeAmbienteEmAberto();
         Processador processador = inicializaProcessador(listBoletos, fatura);
         assertFalse(processador.verificaFaturaPaga());
     }
 
-    @DisplayName("Testa se o processador está criando pagamentos relacionados a fatura")
+    @DisplayName("Testa se o processador estï¿½ criando pagamentos relacionados a fatura")
     @Test
     public void testCriaPagamento() {
+        Fatura fatura = inicializaFaturaDeAmbienteEmAberto();
+        Processador processador = inicializaProcessador(listBoletos,fatura);
         Pagamento pagamento = processador.criaPagamento();
         Assertions.assertAll("processador",
                 () -> assertEquals(400, pagamento.getValor()),
@@ -65,22 +74,13 @@ public class ProcessadorTest {
 
     private Fatura inicializaFaturaDeAmbientePago() {
         //Criando fatura de forma a realizar testes com fatura paga
-        Date date = new Date(System.currentTimeMillis());
-        return new Fatura(date, 400, "Jusicreuza");
+        return new Fatura(DATE, 400, "Jusicreuza");
     }
 
     private Fatura inicializaFaturaDeAmbienteEmAberto() {
         //Criando faruta de forma a realizar testes com fatura em aberto
-        Date date = new Date(System.currentTimeMillis());
-        return new Fatura(date, 800, "Jusicreuza");
+        return new Fatura(DATE, 800, "Jusicreuza");
     }
 
-    private void inicializaBoletos() {
-        List<Boleto> listBoletos = new ArrayList<>();
-        Date date = new Date(System.currentTimeMillis());
-        Boleto boleto = new Boleto("bol123", date, 300);
-        Boleto boleto2 = new Boleto("eto456", date, 100);
-        listBoletos.addAll(Arrays.asList(boleto, boleto2));
-    }
 
 }
